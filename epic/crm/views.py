5dd65874085ddebaadde9f4a69f5addb6e6a34fc
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2022-10-14 10:24:04 +0200

    syncro

--- epic/crm/views.py
+++ epic/crm/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.schemas.openapi import AutoSchema
 from rest_framework.viewsets import GenericViewSet
 
 from .serializers import CustomerListSerializer, CustomerDetailSerializer
@@ -32,7 +33,11 @@ class CustomerViewset(
     delete a customer
 
     """
-
+    schema = AutoSchema(
+        tags=["customer", "crm"],
+        component_name="Customer",
+        operation_id_base="Customer",
+    )
     serializer_class = CustomerListSerializer
     detail_serializer_class = CustomerDetailSerializer
 
